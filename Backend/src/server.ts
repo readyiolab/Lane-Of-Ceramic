7b56958ebec853d890497a4bc6c7713819941ed9
readyiolab <readyiolabs@gmail.com>
2026-06-27 11:35:24 +0530

    Bind backend to 0.0.0.0

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,6 +1,6 @@
 import { app } from "./app.js";
 import { env } from "./config/env.js";
 
-app.listen(env.PORT, () => {
-  console.log(`${env.APP_NAME} running on port ${env.PORT}`);
+app.listen(env.PORT, "0.0.0.0", () => {
+  console.log(`${env.APP_NAME} running on port ${env.PORT} on 0.0.0.0`);
 });
